cffe79c59492eb4243d91aa4975895d41e88b7f0
avaldezp22 <avaldez@igp.gob.pe>
2024-06-11 03:49:58 -0500

    update

--- fc_accuracy.sh
+++ fc_accuracy.sh
@@ -7,3 +7,23 @@ echo "today_temp is $today_temp"
 accuracy=$(($yesterday_sfc-$today_temp))
 echo "accuracy is $accuracy"
 
+if [ -1 -le $accuracy ] && [ $accuracy -le 1 ]
+then
+	accuracy_range=excellent
+elif [ -2 -le $accuracy ] && [ $accuracy -le 2 ]
+then
+	accuracy_range=good
+elif [ -3 -le $accuracy ] && [ $accuracy -le 3 ]
+then
+	accuracy_range=fair 
+else
+	accuracy_range=poor
+fi
+echo "Forecast accuracy is $accuracy"
+
+row=$(tail -1 rx_poc.log)
+year=$( echo $row | cut -d " " -f1)
+month=$( echo $row | cut -d " " -f2)
+day=$( echo $row | cut -d " " -f3)
+echo -e "$year\t$month\t$day\t$today_temp\t$yesterday_sfc\t$accuracy\t$accuracy_range" >> historical_fc_accuracy.tsv
+
